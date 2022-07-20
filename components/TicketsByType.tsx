a8753491b0789ee86927794c6723e0d5f86e49f1
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-07-19 21:54:54 -0400

    removed useStates, useEffects from ticket components. hardcoded options and chart data as consts

--- components/TicketsByType.tsx
+++ components/TicketsByType.tsx
@@ -1,46 +1,38 @@
 import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
 import { Chart as ChartJs, Tooltip, Title, ArcElement, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 ChartJs.register(Tooltip, Title, ArcElement, Legend);
 
+const chartData = {
+  datasets: [
+    {
+      data: [30, 10, 40, 20],
+      backgroundColor: ['#FB3640', '#EFCA08', '#43AA8B', '#253D5B'],
+    },
+  ],
+  labels: [
+    'Bugs/Errors',
+    'Training/Document Requests',
+    'Other Comments',
+    'Feature Requests',
+  ],
+};
+
+const chartOptions = {
+  maintainAspectRatio: false,
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'bottom',
+    },
+    title: {
+      display: true,
+      text: 'Tickets By Priority',
+    },
+  },
+};
+
 const TicketsByType: NextPage = () => {
-  const [chartData, setChartData] = useState({
-    datasets: [],
-  });
-  const [chartOptions, setChartOptions] = useState({});
-
-  useEffect(() => {
-    setChartData({
-      datasets: [
-        {
-          data: [30, 10, 40, 20],
-          backgroundColor: ['#FB3640', '#EFCA08', '#43AA8B', '#253D5B'],
-        },
-      ],
-      labels: [
-        'Bugs/Errors',
-        'Training/Document Requests',
-        'Other Comments',
-        'Feature Requests',
-      ],
-    });
-    setChartOptions({
-      maintainAspectRatio: false,
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'bottom',
-          display: true,
-        },
-        title: {
-          display: true,
-          text: 'Tickets By Priority',
-        },
-      },
-    });
-  }, []);
-
   return (
     <>
       <Pie options={chartOptions} data={chartData} />
