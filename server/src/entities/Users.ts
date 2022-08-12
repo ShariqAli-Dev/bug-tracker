aecff1979b3a0ada98be94b9391ff9420332fb43
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-12 17:30:51 -0400

    created user entity

---
+++ server/src/entities/Users.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field()
+  @Property({ type: "date" })
+  createdAt: Date = new Date();
+
+  @Field()
+  @Property({ type: "date", onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  @Field()
+  @Property({ type: "text", unique: true })
+  email!: string;
+
+  @Property({ type: "text" })
+  password!: string;
+}
