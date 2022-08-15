315afd8710e157caff1e6b347a78f4d1d5d84b7c
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-14 20:41:12 -0400

    converted register page to formik

--- web/pages/register.tsx
+++ web/pages/register.tsx
@@ -18,6 +18,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { FaUserAlt, FaLock, FaBug } from "react-icons/fa";
 import { useRouter } from "next/router";
+import { Form, Formik } from "formik";
 
 const helperTexts = [
   { text: "Have an account?", hyperText: "Sign In", url: "/" },
@@ -25,25 +26,12 @@ const helperTexts = [
 ];
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
-const initalFormValues = {
-  email: "",
-  password: "",
-};
 
 const Register: NextPage = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [formValues, setFormValues] = useState(initalFormValues);
   const router = useRouter();
 
   const handleShowPassword = () => setShowPassword(!showPassword);
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setFormValues({ ...formValues, [e.target.name]: e.target.value });
-
-  const onSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    router.push("/dashboard");
-  };
 
   return (
     <Flex
@@ -73,95 +61,107 @@ const Register: NextPage = () => {
 
         {/* FORM  */}
         <Box minW={{ base: "90%", md: "458px" }}>
-          <form onSubmit={onSubmit}>
-            <Stack spacing={4} p="1rem">
-              {/* EMAIL */}
-              <FormControl>
-                <InputGroup color="#7A7A7A">
-                  <InputLeftElement pointerEvents="none">
-                    <CFaUserAlt />
-                  </InputLeftElement>
-                  <Input
-                    type="email"
-                    placeholder="Email Address"
-                    id="email"
-                    name="email"
-                    onChange={onChange}
-                    autoComplete="email"
-                    required
-                  />
-                </InputGroup>
-              </FormControl>
-              {/* PASSWORD */}
-              <FormControl color="#7A7A7A">
-                <InputGroup>
-                  <InputLeftElement pointerEvents="none">
-                    <CFaLock />
-                  </InputLeftElement>
-                  <Input
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Password"
-                    id="password"
-                    name="password"
-                    onChange={onChange}
-                    autoComplete="current-password"
-                    required
-                  />
-                  <InputRightElement>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            {({ values, handleChange, isSubmitting }) => (
+              <Form>
+                <Stack spacing={4} p="1rem">
+                  {/* EMAIL */}
+                  <FormControl>
+                    <InputGroup color="#7A7A7A">
+                      <InputLeftElement pointerEvents="none">
+                        <CFaUserAlt />
+                      </InputLeftElement>
+                      <Input
+                        type="email"
+                        placeholder="Email Address"
+                        id="email"
+                        name="email"
+                        onChange={handleChange}
+                        value={values.email}
+                        autoComplete="email"
+                        required
+                      />
+                    </InputGroup>
+                  </FormControl>
+                  {/* PASSWORD */}
+                  <FormControl color="#7A7A7A">
+                    <InputGroup>
+                      <InputLeftElement pointerEvents="none">
+                        <CFaLock />
+                      </InputLeftElement>
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Password"
+                        id="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        autoComplete="current-password"
+                        required
+                      />
+                      <InputRightElement>
+                        <Button
+                          width="4.5rem"
+                          size="sm"
+                          onClick={handleShowPassword}
+                        >
+                          {showPassword ? "Hide" : "Show"}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                    {/* SUBMIT LOGIN */}
                     <Button
-                      width="4.5rem"
-                      size="sm"
-                      onClick={handleShowPassword}
+                      borderRadius={0}
+                      type="submit"
+                      backgroundColor="primary"
+                      width="full"
+                      marginTop={5}
+                      marginBottom={5}
+                      rounded="xl"
+                      color="tertiary"
+                      _hover={{
+                        backgroundColor: "tertiary",
+                        color: "primary",
+                        border: "2px",
+                        borderColor: "primary",
+                      }}
+                      isLoading={isSubmitting}
                     >
-                      {showPassword ? "Hide" : "Show"}
+                      Sign Up
                     </Button>
-                  </InputRightElement>
-                </InputGroup>
-                {/* SUBMIT LOGIN */}
-                <Button
-                  borderRadius={0}
-                  type="submit"
-                  backgroundColor="primary"
-                  width="full"
-                  marginTop={5}
-                  marginBottom={5}
-                  rounded="xl"
-                  color="tertiary"
-                  _hover={{
-                    backgroundColor: "tertiary",
-                    color: "primary",
-                    border: "2px",
-                    borderColor: "primary",
-                  }}
-                >
-                  Sign Up
-                </Button>
-                {/* BOTTOM  LINKS */}
-                <FormHelperText textAlign="center">
-                  {helperTexts.map((helperText) => {
-                    return (
-                      <Flex
-                        justifyContent="center"
-                        alignItems="center"
-                        key={helperText.url}
-                        padding=".5rem"
-                        color="primary"
-                      >
-                        <Text marginRight={1} fontSize="md">
-                          {helperText.text}
-                        </Text>
-                        <Text as="u" fontSize="md">
-                          <Link href={helperText.url}>
-                            {helperText.hyperText}
-                          </Link>
-                        </Text>
-                      </Flex>
-                    );
-                  })}
-                </FormHelperText>
-              </FormControl>
-            </Stack>
-          </form>
+                    {/* BOTTOM  LINKS */}
+                    <FormHelperText textAlign="center">
+                      {helperTexts.map((helperText) => {
+                        return (
+                          <Flex
+                            justifyContent="center"
+                            alignItems="center"
+                            key={helperText.url}
+                            padding=".5rem"
+                            color="primary"
+                          >
+                            <Text marginRight={1} fontSize="md">
+                              {helperText.text}
+                            </Text>
+                            <Text as="u" fontSize="md">
+                              <Link href={helperText.url}>
+                                {helperText.hyperText}
+                              </Link>
+                            </Text>
+                          </Flex>
+                        );
+                      })}
+                    </FormHelperText>
+                  </FormControl>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
         </Box>
       </Stack>
     </Flex>
