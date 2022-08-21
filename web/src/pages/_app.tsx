efce3d6324f38437c9eb840f66df917847047703
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-21 17:36:08 -0400

    token auth info now persists after refresh

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -3,7 +3,8 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider, createClient } from "urql";
 import theme from "../theme";
-import { getAccessToken } from "../accessTokens";
+import { getAccessToken, setAccessToken } from "../accessTokens";
+import { useEffect, useState } from "react";
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
@@ -18,7 +19,24 @@ const client = createClient({
   },
 });
 
-function MyApp({ Component, pageProps }: AppProps) {
+function BugTracker({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    }).then(async (data) => {
+      const { accessToken } = await data.json();
+      setAccessToken(accessToken);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
   return (
     <Provider value={client}>
       <ChakraProvider theme={theme}>
@@ -28,4 +46,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default BugTracker;
