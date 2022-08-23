7898c2bb79e23d73135a25e8a10fe7532ba44fe4
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-22 21:18:56 -0400

    added dynamic access token refresh

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Provider, createClient } from "urql";
 import theme from "../theme";
 import { getAccessToken, setAccessToken } from "../accessTokens";
 import { useEffect, useState } from "react";
+import jwt from "jsonwebtoken";
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
@@ -23,14 +24,42 @@ function BugTracker({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:4000/refresh-token", {
-      method: "POST",
-      credentials: "include",
-    }).then(async (data) => {
-      const { accessToken } = await data.json();
-      setAccessToken(accessToken);
-      setLoading(false);
-    });
+    // if accessToken doesn't exist, get it
+    if (!getAccessToken()) {
+      fetch("http://localhost:4000/refresh-token", {
+        method: "POST",
+        credentials: "include",
+      }).then(async (data) => {
+        const { accessToken } = await data.json();
+        setAccessToken(accessToken);
+        setLoading(false);
+      });
+    } else {
+      const decoded: any = jwt.verify(
+        getAccessToken(),
+        process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET as string
+      );
+      const date = parseInt(
+        new Date()
+          .getTime()
+          .toString()
+          .substring(0, decoded.exp.toString().length)
+      );
+      if (date - decoded.exp <= 120) {
+        // result is in seconds
+        fetch("http://localhost:4000/refresh-token", {
+          method: "POST",
+          credentials: "include",
+        }).then(async (data) => {
+          const { accessToken } = await data.json();
+          setAccessToken(accessToken);
+          setLoading(false);
+        });
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
+    }
   }, []);
 
   if (loading) {
