d2b171c9acd29386ce94c7216509e2ce72565f50
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-24 20:57:03 -0400

    converted forgot password into formik fomr

--- web/src/pages/forgot-password/index.tsx
+++ web/src/pages/forgot-password/index.tsx
@@ -15,22 +15,12 @@ import {
   Text,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import React, { useState } from "react";
 import { FaUserAlt } from "react-icons/fa";
+import { Formik } from "formik";
 
 const CFaUserAlt = chakra(FaUserAlt);
-const initalFormValues = { email: "" };
 
 const ForgotPassword: NextPage = () => {
-  const [formValues, setFormValues] = useState(initalFormValues);
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setFormValues({ ...formValues, [e.target.name]: e.target.value });
-
-  const onSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-  };
-
   return (
     <Flex
       flexDirection="column"
@@ -57,56 +47,69 @@ const ForgotPassword: NextPage = () => {
             Please enter the email address you&apos;d like your password reset
             information sent to
           </Text>
-          <form onSubmit={onSubmit}>
-            <Stack spacing={4} p="1rem">
-              <FormControl>
-                <FormLabel>Enter Email Address</FormLabel>
-                <InputGroup>
-                  <InputLeftElement pointerEvents="none">
-                    <CFaUserAlt color="gray.500" />
-                  </InputLeftElement>
-                  <Input
-                    boxShadow="md"
-                    name="email"
-                    id="email"
-                    type="email"
-                    borderColor="blue"
-                    autoComplete="email"
-                    onChange={onChange}
-                    required
-                  />
-                </InputGroup>
-              </FormControl>
-              <Button
-                borderRadius={0}
-                type="submit"
-                variant="solid"
-                colorScheme="facebook"
-                width="full"
-                marginTop={5}
-                marginBottom={5}
-                rounded="xl"
-              >
-                Request Reset Link
-              </Button>
-              <FormControl>
-                <FormHelperText>
-                  <Flex
-                    justifyContent="center"
-                    alignItems="center"
-                    padding=".5rem"
-                  >
-                    <Text color="blackAlpha.700" marginRight={1} fontSize="md">
-                      Take me back to
-                    </Text>
-                    <Text color="blackAlpha.700" as="u" fontSize="md">
-                      <Link href="/">Sign In</Link>
-                    </Text>
-                  </Flex>
-                </FormHelperText>
-              </FormControl>
-            </Stack>
-          </form>
+          <Formik
+            initialValues={{ email: "" }}
+            onSubmit={() => {
+              console.log("i do be submitting");
+            }}
+          >
+            {({ values, handleChange, isSubmitting }) => (
+              <Stack spacing={4} p="1rem">
+                <FormControl>
+                  <FormLabel>Enter Email Address</FormLabel>
+                  <InputGroup>
+                    <InputLeftElement pointerEvents="none">
+                      <CFaUserAlt color="gray.500" />
+                    </InputLeftElement>
+                    <Input
+                      boxShadow="md"
+                      name="email"
+                      id="email"
+                      type="email"
+                      borderColor="blue"
+                      autoComplete="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      required
+                    />
+                  </InputGroup>
+                </FormControl>
+                <Button
+                  borderRadius={0}
+                  type="submit"
+                  variant="solid"
+                  colorScheme="facebook"
+                  width="full"
+                  marginTop={5}
+                  marginBottom={5}
+                  rounded="xl"
+                  isLoading={isSubmitting}
+                >
+                  Request Reset Link
+                </Button>
+                <FormControl>
+                  <FormHelperText>
+                    <Flex
+                      justifyContent="center"
+                      alignItems="center"
+                      padding=".5rem"
+                    >
+                      <Text
+                        color="blackAlpha.700"
+                        marginRight={1}
+                        fontSize="md"
+                      >
+                        Take me back to
+                      </Text>
+                      <Text color="blackAlpha.700" as="u" fontSize="md">
+                        <Link href="/">Sign In</Link>
+                      </Text>
+                    </Flex>
+                  </FormHelperText>
+                </FormControl>
+              </Stack>
+            )}
+          </Formik>
         </Box>
       </Stack>
     </Flex>
