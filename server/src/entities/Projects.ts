06d4261675368eaad61c0904d99be2904dcd4e62
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-29 14:58:44 -0400

    created Projects entity

---
+++ server/src/entities/Projects.ts
@@ -0,0 +1,33 @@
+import { Field, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Project extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @Column()
+  name!: string;
+
+  @Field()
+  @Column()
+  description!: string;
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
