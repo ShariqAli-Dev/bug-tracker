3f8006a9978c0fe4165e8467ebc64a3b02f7aaaf
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-31 13:37:29 -0400

    createProject & updateProject mutation

--- server/src/resolvers/project.ts
+++ server/src/resolvers/project.ts
@@ -14,9 +14,36 @@ export class ProjectResolver {
   }
 
   @Mutation(() => Project)
-  async createProject() // description // name
-  //
-  {
-    return {};
+  async createProject(
+    @Arg("description") description: string,
+    @Arg("name") name: string
+  ): Promise<Project> {
+    if (!name || !description) {
+      throw new Error("invalid input");
+    }
+
+    return await Project.create({
+      description,
+      name,
+    }).save();
+  }
+
+  @Mutation(() => Project, { nullable: true })
+  async updateProject(
+    @Arg("options") options: { id: number; name: string; description: string }
+  ): Promise<Project | null> {
+    const { id, name, description } = options;
+    const project = await Project.findOne({ where: { id } });
+    if (!project) return null;
+
+    if (
+      typeof id !== "undefined" &&
+      typeof name !== "undefined" &&
+      typeof description !== "undefined"
+    ) {
+      Project.update({ id }, { name, description });
+    }
+
+    return project;
   }
 }
