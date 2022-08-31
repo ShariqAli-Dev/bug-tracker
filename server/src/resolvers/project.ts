b53132be437b5d0c234be051bb8cbc8450d4e653
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-08-31 13:20:09 -0400

    created projects() endpoint in resolver

--- server/src/resolvers/project.ts
+++ server/src/resolvers/project.ts
@@ -3,14 +3,18 @@ import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class ProjectResolver {
+  @Query(() => [Project])
+  async projects(): Promise<Project[]> {
+    return await Project.find();
+  }
+
   @Query(() => Project, { nullable: true })
   async project(@Arg("id") id: number): Promise<Project | null> {
     return await Project.findOne({ where: { id } });
   }
 
   @Mutation(() => Project)
-  async createProject() // name
-  // description
+  async createProject() // description // name
   //
   {
     return {};
