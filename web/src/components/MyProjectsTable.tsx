d1a0848ccd15a915be4f1d78e6ee0bd48697c742
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-09-14 21:26:31 -0400

    bugfix: fixed infinite calls glitch

--- web/src/components/MyProjectsTable.tsx
+++ web/src/components/MyProjectsTable.tsx
@@ -25,13 +25,12 @@ import {
 import { Form, Formik } from "formik";
 import { nanoid } from "nanoid";
 import { withUrqlClient } from "next-urql";
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
 import { usePagination, useTable } from "react-table";
 import {
   useCreateProjectMutation,
-  User_Project,
   useUserProjectsQuery,
 } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
@@ -42,9 +41,9 @@ const ChevronRight = chakra(BsChevronDoubleRight);
 const ChevronLeft = chakra(BsChevronDoubleLeft);
 
 const MyProjectsTable = () => {
+  const [tableData, setTableData] = useState([]);
   const toast = useToast();
-  const [{ data, fetching, error }] = useUserProjectsQuery();
-  console.log(data?.UserProjects);
+  const [{ data, fetching }] = useUserProjectsQuery();
   const initialRef = useRef(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const finalRef = useRef(null);
@@ -52,7 +51,6 @@ const MyProjectsTable = () => {
     { name: "shariq", selected: false },
     { name: "john", selected: false },
   ]);
-
   const [, createProject] = useCreateProjectMutation();
   const columns = useMemo(
     () => [
@@ -76,7 +74,6 @@ const MyProjectsTable = () => {
     ],
     []
   );
-
   const {
     // getTableProps,
     getTableBodyProps,
@@ -94,146 +91,158 @@ const MyProjectsTable = () => {
   } = useTable(
     {
       columns,
-      data: data?.UserProjects as any,
+      data: tableData,
       initialState: { pageIndex: 0, pageSize: 5 },
     },
     usePagination
   );
 
+  useEffect(() => {
+    if (!fetching) {
+      setTableData(data?.UserProjects as any);
+    } else {
+      return;
+    }
+  }, [data, fetching]);
+
   return (
     <>
-      <TableContainer whiteSpace="normal" style={{ width: "90%" }}>
-        <Flex
-          justifyContent="space-between"
-          alignItems="center"
-          backgroundColor="primary"
-          color="tertiary"
-          w="full"
-        >
-          <Box margin="6px" ref={finalRef}>
-            <Text>Your Projects</Text>
-            <Text display={{ base: "none", md: "block" }}>
-              Alll the projects you have in the database
+      {!fetching && (
+        <>
+          <TableContainer whiteSpace="normal" style={{ width: "90%" }}>
+            <Flex
+              justifyContent="space-between"
+              alignItems="center"
+              backgroundColor="primary"
+              color="tertiary"
+              w="full"
+            >
+              <Box margin="6px" ref={finalRef}>
+                <Text>Your Projects</Text>
+                <Text display={{ base: "none", md: "block" }}>
+                  Alll the projects you have in the database
+                </Text>
+              </Box>
+              <Button
+                marginRight="6px"
+                color="primary"
+                backgroundColor="tertiary"
+                border="1px"
+                borderColor="primary"
+                onClick={onOpen}
+              >
+                Create New Project
+              </Button>
+            </Flex>
+
+            <Table
+              {...getTableBodyProps()}
+              size={{ base: "sm", lg: "md" }}
+              border="2px"
+              borderColor="primary"
+            >
+              <Thead border="2px" borderColor="primary">
+                {headerGroups.map((headerGroup) => (
+                  <Tr {...headerGroup.getHeaderGroupProps()} key={nanoid()}>
+                    {headerGroup.headers
+                      .filter((header) => header.Header !== "My Projects")
+                      .map((column) => (
+                        <Th
+                          {...column.getHeaderProps()}
+                          key={nanoid()}
+                          border="2px"
+                          borderColor="primary"
+                        >
+                          {column.render("Header")}
+                        </Th>
+                      ))}
+                  </Tr>
+                ))}
+              </Thead>
+              <Tbody {...getTableBodyProps()}>
+                {page.map((row) => {
+                  prepareRow(row);
+                  return (
+                    <Tr {...row.getRowProps()} key={nanoid()}>
+                      {row.cells.map((cell) => {
+                        return (
+                          <Td
+                            {...cell.getCellProps()}
+                            key={nanoid()}
+                            border="2px"
+                            borderColor="primary"
+                          >
+                            {cell.render("Cell")}
+                            <div>Details</div>
+                          </Td>
+                        );
+                      })}
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </TableContainer>
+          <Flex justifyContent="center" alignItems="center">
+            {/* Chevron Left */}
+            <Tooltip label="First Page">
+              <IconButton
+                color="primary"
+                aria-label="Chevron Left"
+                onClick={() => gotoPage(0)}
+                isDisabled={!canPreviousPage}
+                icon={<ChevronLeft size="20px" />}
+              />
+            </Tooltip>
+            {/* Arrow Left */}
+            <Tooltip label="Previous Page">
+              <IconButton
+                color="primary"
+                aria-label="Arrow Left"
+                onClick={previousPage}
+                isDisabled={!canPreviousPage}
+                icon={<ArrowLeft size="20px" />}
+              />
+            </Tooltip>
+
+            {/* Page Tracker */}
+            <Text
+              fontWeight="bold"
+              color="tertiary"
+              backgroundColor="primary"
+              border="2px"
+              borderRadius="20px"
+              padding=".5rem"
+              margin="1rem"
+              width="45px"
+              textAlign="center"
+            >
+              {pageIndex + 1}
             </Text>
-          </Box>
-          <Button
-            marginRight="6px"
-            color="primary"
-            backgroundColor="tertiary"
-            border="1px"
-            borderColor="primary"
-            onClick={onOpen}
-          >
-            Create New Project
-          </Button>
-        </Flex>
 
-        <Table
-          {...getTableBodyProps()}
-          size={{ base: "sm", lg: "md" }}
-          border="2px"
-          borderColor="primary"
-        >
-          <Thead border="2px" borderColor="primary">
-            {headerGroups.map((headerGroup) => (
-              <Tr {...headerGroup.getHeaderGroupProps()} key={nanoid()}>
-                {headerGroup.headers
-                  .filter((header) => header.Header !== "My Projects")
-                  .map((column) => (
-                    <Th
-                      {...column.getHeaderProps()}
-                      key={nanoid()}
-                      border="2px"
-                      borderColor="primary"
-                    >
-                      {column.render("Header")}
-                    </Th>
-                  ))}
-              </Tr>
-            ))}
-          </Thead>
-          <Tbody {...getTableBodyProps()}>
-            {page.map((row) => {
-              prepareRow(row);
-              return (
-                <Tr {...row.getRowProps()} key={nanoid()}>
-                  {row.cells.map((cell) => {
-                    return (
-                      <Td
-                        {...cell.getCellProps()}
-                        key={nanoid()}
-                        border="2px"
-                        borderColor="primary"
-                      >
-                        {cell.render("Cell")}
-                        <div>Details</div>
-                      </Td>
-                    );
-                  })}
-                </Tr>
-              );
-            })}
-          </Tbody>
-        </Table>
-      </TableContainer>
-      <Flex justifyContent="center" alignItems="center">
-        {/* Chevron Left */}
-        <Tooltip label="First Page">
-          <IconButton
-            color="primary"
-            aria-label="Chevron Left"
-            onClick={() => gotoPage(0)}
-            isDisabled={!canPreviousPage}
-            icon={<ChevronLeft size="20px" />}
-          />
-        </Tooltip>
-        {/* Arrow Left */}
-        <Tooltip label="Previous Page">
-          <IconButton
-            color="primary"
-            aria-label="Arrow Left"
-            onClick={previousPage}
-            isDisabled={!canPreviousPage}
-            icon={<ArrowLeft size="20px" />}
-          />
-        </Tooltip>
-
-        {/* Page Tracker */}
-        <Text
-          fontWeight="bold"
-          color="tertiary"
-          backgroundColor="primary"
-          border="2px"
-          borderRadius="20px"
-          padding=".5rem"
-          margin="1rem"
-          width="45px"
-          textAlign="center"
-        >
-          {pageIndex + 1}
-        </Text>
-
-        {/* Chevron Right */}
-        <Tooltip label="Next Page">
-          <IconButton
-            color="primary"
-            aria-label="Arrow Right"
-            onClick={nextPage}
-            isDisabled={!canNextPage}
-            icon={<ArrowRight size="20px" />}
-          />
-        </Tooltip>
-        {/* Arrow Right */}
-        <Tooltip label="Last Page">
-          <IconButton
-            color="primary"
-            aria-label="Chevron Right"
-            onClick={() => gotoPage(pageCount - 1)}
-            isDisabled={!canNextPage}
-            icon={<ChevronRight size="20px" />}
-          />
-        </Tooltip>
-      </Flex>
+            {/* Chevron Right */}
+            <Tooltip label="Next Page">
+              <IconButton
+                color="primary"
+                aria-label="Arrow Right"
+                onClick={nextPage}
+                isDisabled={!canNextPage}
+                icon={<ArrowRight size="20px" />}
+              />
+            </Tooltip>
+            {/* Arrow Right */}
+            <Tooltip label="Last Page">
+              <IconButton
+                color="primary"
+                aria-label="Chevron Right"
+                onClick={() => gotoPage(pageCount - 1)}
+                isDisabled={!canNextPage}
+                icon={<ChevronRight size="20px" />}
+              />
+            </Tooltip>
+          </Flex>
+        </>
+      )}
       <Modal
         initialFocusRef={initialRef}
         finalFocusRef={finalRef}
