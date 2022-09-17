0c91ccb86178596162da50c50a9dffc4c656d253
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-09-17 15:34:09 -0400

    updated graphql queries to include queriesBy

--- web/src/components/TicketsByPriority.tsx
+++ web/src/components/TicketsByPriority.tsx
@@ -1,11 +1,11 @@
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
   BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
   Title,
   Tooltip,
-  Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
 ChartJS.register(
