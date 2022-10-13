cb9f978a8668f47454bf8d4f7945830e7689923e
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-10-13 19:55:11 -0400

    created ticketComment query

--- server/src/resolvers/comment.ts
+++ server/src/resolvers/comment.ts
@@ -1,15 +1,17 @@
-import { Comment } from "../entities/Comment";
 import {
   Arg,
   Ctx,
   Field,
   InputType,
   Mutation,
+  ObjectType,
   Query,
   Resolver,
 } from "type-graphql";
-import { MyContext } from "src/types";
 import { myDataSource } from "../data-source";
+import { Comment } from "../entities/Comment";
+import { Users } from "../entities/Users";
+import { MyContext } from "../types";
 @InputType()
 class createComment {
   @Field()
@@ -19,11 +21,43 @@ class createComment {
   message!: string;
 }
 
+@ObjectType()
+class TicketComment {
+  @Field()
+  id: number;
+
+  @Field()
+  message: string;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  user: Users;
+}
+
 @Resolver()
 export class CommentResolver {
   @Query(() => [Comment])
   async comments(): Promise<Comment[]> {
     return await Comment.find();
+  }
+
+  @Query(() => [TicketComment])
+  async ticketComments(
+    @Arg("ticketId") ticketId: number
+  ): Promise<TicketComment[]> {
+    return await myDataSource.query(
+      `select "c".*,
+        json_build_object(
+          'id', u.id,
+          'name', u.name,
+          'createdAt', u."createdAt"
+        ) "user"
+      from "comment" "c"
+      inner join users u on u.id = "c"."userId"
+      where c."ticketId" = ${ticketId}`
+    );
   }
 
   @Mutation(() => Boolean)
