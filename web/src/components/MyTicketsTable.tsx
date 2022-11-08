cd8bf461a3608dee614423a3cbd3dac7efb8c332
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-11-07 23:17:35 -0500

    created ticketa table

---
+++ web/src/components/MyTicketsTable.tsx
@@ -0,0 +1,217 @@
+import {
+  chakra,
+  Flex,
+  IconButton,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tooltip,
+  Tr,
+} from "@chakra-ui/react";
+import { nanoid } from "nanoid";
+import Link from "next/link";
+import { useMemo } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
+import { usePagination, useTable } from "react-table";
+import { useUserTicketsQuery } from "../generated/graphql";
+const ArrowRight = chakra(AiOutlineArrowRight);
+const ArrowLeft = chakra(AiOutlineArrowLeft);
+const ChevronRight = chakra(BsChevronDoubleRight);
+const ChevronLeft = chakra(BsChevronDoubleLeft);
+
+const MyTicketsTable = () => {
+  const [{ data, fetching }] = useUserTicketsQuery();
+  const columns = useMemo(
+    () => [
+      {
+        Header: "My Tickets",
+        columns: [
+          {
+            Header: "Project",
+            accessor: "name",
+          },
+          {
+            Header: "Ticket",
+            accessor: "title",
+          },
+          {
+            Header: "Status",
+            accessor: "status",
+          },
+          {
+            Header: "Priority",
+            accessor: "priority",
+          },
+          {
+            Header: "",
+            accessor: "view ticket",
+          },
+        ],
+      },
+    ],
+    []
+  );
+  const {
+    // getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    prepareRow,
+    page,
+    canPreviousPage,
+    canNextPage,
+    // pageOptions,`
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    state: { pageIndex },
+  } = useTable(
+    {
+      columns,
+      data: data?.userTickets as any,
+      initialState: { pageIndex: 0, pageSize: 5 },
+    },
+    usePagination
+  );
+  if (fetching) {
+    return <></>;
+  }
+  return (
+    <>
+      <TableContainer marginTop={"5rem"}>
+        <Table
+          {...getTableBodyProps()}
+          size={{ base: "sm", sm: "md", md: "lg" }}
+          border="2px"
+          borderColor="primary"
+        >
+          <Thead border="2px" borderColor="primary">
+            {headerGroups.map((headerGroup, hdx) => (
+              <Tr
+                {...headerGroup.getHeaderGroupProps()}
+                key={`headerGroup${hdx}`}
+              >
+                {headerGroup.headers
+                  .filter((header) => header.Header !== "My Tickets")
+                  .map((column, cdx) => {
+                    return (
+                      <Th
+                        {...column.getHeaderProps()}
+                        key={cdx}
+                        border="2px"
+                        borderColor="primary"
+                      >
+                        {column.render("Header")}
+                      </Th>
+                    );
+                  })}
+              </Tr>
+            ))}
+          </Thead>
+          <Tbody {...getTableBodyProps()}>
+            {page.map((row, rdx) => {
+              prepareRow(row);
+              return (
+                <Tr {...row.getRowProps()} key={rdx}>
+                  {row.cells.map((cell) => {
+                    const definiteData = data?.userTickets as any;
+                    return (
+                      <Td
+                        {...cell.getCellProps()}
+                        key={nanoid()}
+                        border="2px"
+                        borderColor="primary"
+                      >
+                        {cell.render("Cell")}
+                        {!cell.column.Header && (
+                          <Link
+                            href={`/project/${parseInt(
+                              definiteData[parseInt(row.id)].projectId as any
+                            )}`}
+                          >
+                            <Text
+                              textDecoration="underline"
+                              cursor="pointer"
+                              textAlign="center"
+                            >
+                              View Ticket
+                            </Text>
+                          </Link>
+                        )}
+                      </Td>
+                    );
+                  })}
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <Flex justifyContent="center" alignItems="center" marginTop="4rem">
+        {/* Chevron Left */}
+        <Tooltip label="First Page">
+          <IconButton
+            color="primary"
+            aria-label="Chevron Left"
+            onClick={() => gotoPage(0)}
+            isDisabled={!canPreviousPage}
+            icon={<ChevronLeft size="20px" />}
+          />
+        </Tooltip>
+        {/* Arrow Left */}
+        <Tooltip label="Previous Page">
+          <IconButton
+            color="primary"
+            aria-label="Arrow Left"
+            onClick={previousPage}
+            isDisabled={!canPreviousPage}
+            icon={<ArrowLeft size="20px" />}
+          />
+        </Tooltip>
+
+        {/* Page Tracker */}
+        <Text
+          fontWeight="bold"
+          color="tertiary"
+          backgroundColor="primary"
+          border="2px"
+          borderRadius="20px"
+          padding=".5rem"
+          margin="1rem"
+          width="45px"
+          textAlign="center"
+        >
+          {pageIndex + 1}
+        </Text>
+
+        {/* Chevron Right */}
+        <Tooltip label="Next Page">
+          <IconButton
+            color="primary"
+            aria-label="Arrow Right"
+            onClick={nextPage}
+            isDisabled={!canNextPage}
+            icon={<ArrowRight size="20px" />}
+          />
+        </Tooltip>
+        {/* Arrow Right */}
+        <Tooltip label="Last Page">
+          <IconButton
+            color="primary"
+            aria-label="Chevron Right"
+            onClick={() => gotoPage(pageCount - 1)}
+            isDisabled={!canNextPage}
+            icon={<ChevronRight size="20px" />}
+          />
+        </Tooltip>
+      </Flex>
+    </>
+  );
+};
+
+export default MyTicketsTable;
