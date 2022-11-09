6c8485b3d4b09c69ea10051fd0ee46390beac653
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-11-08 22:16:09 -0500

    adminstration page dynamicaly renders users

--- web/src/pages/adminstration.tsx
+++ web/src/pages/adminstration.tsx
@@ -1,11 +1,22 @@
 import { Box, Container, Flex, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import { withUrqlClient } from "next-urql";
+import { useState } from "react";
 import BurgerMenu from "../components/BurgerMenu";
 import NavBar from "../components/Navbar";
+import { useUsersQuery, Users } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Adminstration: NextPage = () => {
+  const [{ data: users, fetching }] = useUsersQuery();
+  const [selectedUser, setSelectedUser] = useState<Users | undefined>(
+    undefined
+  );
+
+  if (fetching) {
+    return <></>;
+  }
+
   return (
     <Flex
       h={{ base: "140vh", md: "100vh" }}
@@ -47,7 +58,7 @@ const Adminstration: NextPage = () => {
           height="40%"
         >
           <Box height="full" width={{ base: "85%", md: "45%" }}>
-            <Text>Organization</Text>
+            <Text>Organization</Text>{" "}
             <Box
               overflowY="auto"
               scrollBehavior="auto"
@@ -56,12 +67,30 @@ const Adminstration: NextPage = () => {
               borderColor="light-blue"
               borderWidth={0.1}
               borderRadius={"xl"}
+              paddingTop="1rem"
             >
-              testng
+              {users?.users.map((u) => (
+                <Flex
+                  width="full"
+                  justifyContent={"space-between"}
+                  fontSize={"1rem"}
+                  key={u.email}
+                  cursor="pointer"
+                >
+                  <Box
+                    display="flex"
+                    justifyContent="space-around"
+                    width="full"
+                  >
+                    <Text>{u.name}</Text>
+                    <Text>{u.email}</Text>
+                  </Box>
+                </Flex>
+              ))}
             </Box>
           </Box>
           <Box height="full" width={{ base: "85%", md: "45%" }}>
-            <Text>Edit User Information</Text>{" "}
+            <Text>Edit User Information</Text>
             <Box
               overflowY="auto"
               scrollBehavior="auto"
@@ -70,6 +99,7 @@ const Adminstration: NextPage = () => {
               borderColor="light-blue"
               borderWidth={0.1}
               borderRadius={"xl"}
+              paddingTop="1rem"
             >
               testng
             </Box>
