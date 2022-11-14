c6bc6151687274a035ea1ea77adf0f9ceae16cae
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-11-13 20:00:19 -0500

    bugfix: creating a ticket invalidates the userTickets chache

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -149,7 +149,9 @@ export const createUrqlClient = (ssrExchange: any) => {
             createTicket: (_result, args, cache, info) => {
               const allFields = cache.inspectFields("Query");
               const fieldInfos = allFields.filter(
-                (info) => info.fieldName === "projectTickets"
+                (info) =>
+                  info.fieldName === "projectTickets" ||
+                  info.fieldName === "userTickets"
               );
               fieldInfos.forEach((fi) => {
                 cache.invalidate("Query", fi.fieldName, fi.arguments);
