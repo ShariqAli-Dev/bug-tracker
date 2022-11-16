0b6063e0d3f626dc8bd9907c2973013f4ca56356
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-11-15 22:00:31 -0500

    bugfix: demo password was missing string quotations

--- server/src/migrations/1668566475322-migrations.ts
+++ server/src/migrations/1668566475322-migrations.ts
@@ -4,13 +4,13 @@ import { DEMO_USER_PASSWORD } from "../constants";
 export class migrations1668566475322 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.query(`
-    insert into users (id, email, role, password, name) values (1, 'demoAdmin@bugtracker.com', 'admin', ${DEMO_USER_PASSWORD}, 'admin');
+    insert into users (id, email, role, password, name) values (1, 'demoAdmin@bugtracker.com', 'admin', '${DEMO_USER_PASSWORD}', 'admin');
     
-    insert into users (id, email, role, password, name) values (2, 'demoProjectManager@bugtracker.com', 'project manager', ${DEMO_USER_PASSWORD}, 'project manager');
+    insert into users (id, email, role, password, name) values (2, 'demoProjectManager@bugtracker.com', 'project manager', '${DEMO_USER_PASSWORD}', 'project manager');
     
-    insert into users (id, email, role, password, name) values (3, 'demoDeveloper@bugtracker.com', 'developer', ${DEMO_USER_PASSWORD}, 'developer');
+    insert into users (id, email, role, password, name) values (3, 'demoDeveloper@bugtracker.com', 'developer', '${DEMO_USER_PASSWORD}', 'developer');
     
-    insert into users (id, email, role, password, name) values (4, 'demoSubmitter@bugtracker.com', 'submitter', ${DEMO_USER_PASSWORD}, 'submitter');`);
+    insert into users (id, email, role, password, name) values (4, 'demoSubmitter@bugtracker.com', 'submitter', '${DEMO_USER_PASSWORD}', 'submitter');`);
   }
 
   public async down(_: QueryRunner): Promise<void> {}
