4770c1b661ef9490a255b07f37ee74182bcdb5b4
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-11-20 22:43:38 -0500

    genereted a .env example with types

---
+++ server/src/env.d.ts
@@ -0,0 +1,19 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PG_PASSWORD: string;
+      DB_NAME: string;
+      ACCESS_TOKEN_SECRET: string;
+      REFRESH_TOKEN_SECRET: string;
+      PASSWORD_RESET_SECRET: string;
+      REDIS_SECRET: string;
+      COOKIE_NAME: string;
+      DEMO_USER_PASSWORD: string;
+      DATABASE_URL: string;
+      REDIS_URL: string;
+      PORT: string;
+    }
+  }
+}
+
+export {}
