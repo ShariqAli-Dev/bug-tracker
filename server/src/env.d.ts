841fb60b5d4166052b7baf25d2b6488b02543a4e
ShariqAli-Dev <shariqali.dev@gmail.com>
2022-11-23 21:54:49 -0500

    added docker and .env specifications for hosting the backend with docker

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -3,16 +3,15 @@ declare global {
     interface ProcessEnv {
       PG_PASSWORD: string;
       DB_NAME: string;
-      ACCESS_TOKEN_SECRET: string;
-      REFRESH_TOKEN_SECRET: string;
+      POSTGRES_USERNAME: string;
       PASSWORD_RESET_SECRET: string;
+      DATABASE_URL: string;
       REDIS_SECRET: string;
       COOKIE_NAME: string;
       DEMO_USER_PASSWORD: string;
-      DATABASE_URL: string;
       REDIS_URL: string;
       PORT: string;
-      POSTGRES_USERNAME: string;
+      CORS_ORIGIN: string;
     }
   }
 }
