a04ecca1e01f6d45c6ebedaa568d19b874ff7b45
shariqali-dev <shariqali.dev@gmail.com>
2024-04-29 12:11:17 -0400

    enhan: analytics

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import theme from "../theme";
+import { Analytics } from "@vercel/analytics/react";
 
 function BugTracker({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
